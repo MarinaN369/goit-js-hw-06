28eaadd0d65eac2bd92e6a13c888c27b2613522e
MarinaN369 <143936204+MarinaN369@users.noreply.github.com>
2023-12-27 13:03:20 +0200

    hw-js-06-виправлення-2

--- js/task-2.js
+++ js/task-2.js
@@ -1,10 +1,16 @@
-class Storage { constructor(items) { this.items = items; }
+class Storage {
+    #items;
 
-getItems() { return this.items; }
+    constructor(items) {this.#items = items;}
 
-addItem(newItem) { this.items.push(newItem); }
+    getItems()
+    {return this.#items;}
 
-    removeItem(itemToRemove) { const index = this.items.indexOf(itemToRemove); if (index !== -1) { this.items.splice(index, 1); } }
+    addItem(newItem)
+    {this.#items.push(newItem);}
+
+    removeItem(itemToRemove)
+    {const index = this.#items.indexOf(itemToRemove); if (index !== -1) {this.#items.splice(index, 1);}}
 }
 
 const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
